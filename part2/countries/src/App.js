a3a3c60c08bd476a944086560f3a676137aaaca4
Jose Angulo <jjavier1708@gmail.com>
2021-12-30 16:48:35 -0700

    Create the countries app

---
+++ part2/countries/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello World!!!</p>
+    </div>
+  );
+}
+
+export default App;
