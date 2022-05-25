976f89fd3e96fbb464aa59449edef55fb8d09060
Jose Angulo <jjavier1708@gmail.com>
2022-05-24 17:32:54 -0700

    Exercise 4.8 (step1)

---
+++ part4/blog-list/tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: '3 Quarks Daily',
+    author: 'S. Abbas Raza',
+    url: 'www.3quarksdaily.com',
+    likes: 26,
+  },
+  {
+    title: 'Airline Reporter',
+    author: 'David Parker Brown',
+    url: 'www.airlinereporter.com',
+    likes: 65,
+  },
+]
+
+module.exports = {
+  initialBlogs
+}
