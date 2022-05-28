197239be5dcaaac743a577ae8432d7031ae8adf8
Jose Angulo <jjavier1708@gmail.com>
2022-05-27 17:59:50 -0700

    Exercises 4.9-4.12

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -7,7 +7,17 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+  })
+
+  if (!request.body.hasOwnProperty('title') && !request.body.hasOwnProperty('url')) {
+    return response.status(400).json({ message: 'No title & url' })
+  }
+
   const result = await blog.save()
   response.status(201).json(result)
 })
--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -27,6 +27,64 @@ test('all notes are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier property of the blog posts is named id', async () => {
+  const blogs = await (await api.get('/api/blogs')).body
+  const filteredBlogs = blogs.filter(blog => blog.hasOwnProperty('id'))
+  expect(filteredBlogs).toBeDefined()
+})
+
+test('successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: 'Belle de Jour',
+    author: 'Brooke Magnanti',
+    url: 'belledejour.substack.com',
+    likes: 12,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(n => n.url)
+  expect(contents).toContain('belledejour.substack.com')
+})
+
+test('verifies that if the likes property is missing from the request, it will default to the value 0', async () => {
+  const newBlog = {
+    title: '1000 Awesome Things',
+    author: 'Neil Pasricha',
+    url: 'www.1000awesomethings.com',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const blogCreated = blogsAtEnd.filter(blog => blog.url=== 'www.1000awesomethings.com')
+  expect(blogCreated[0].likes).toBe(0)
+})
+
+test('verifies that if the title and url properties are missing from the request data, the backend responds with status 400', async () => {
+  const newBlog = {
+    author: 'Neil Pasricha',
+    likes: 65
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
